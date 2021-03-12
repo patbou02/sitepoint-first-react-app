685008b949b27b923ddc62076b1681376b638d51
Patrick Boutin <patrick.boutin@roberthalf.com>
2021-03-11 23:12:44 -0800

    Added info cards and imported external CSS file.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 
 class App extends React.Component {
   render() {
@@ -7,7 +8,27 @@ class App extends React.Component {
         <div className="row">
           <div className="col-md-4">
             <div className="card">
-              <img className="card-img-top" src="..." alt="Card image cap" />
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
               <div className="card-body">
                 <h5 className="card-title">Card title</h5>
                 <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
