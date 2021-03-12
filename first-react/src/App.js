f8f678a194279732f31dafdb941f91833aedb9c6
Patrick Boutin <patrick.boutin@roberthalf.com>
2021-03-11 23:03:13 -0800

    Installed Bootstrap and updated Class component.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -3,7 +3,20 @@ import React from 'react';
 class App extends React.Component {
   render() {
     return (
-      <h1>Patrick Boutin</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <div className="card">
+              <img className="card-img-top" src="..." alt="Card image cap" />
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
