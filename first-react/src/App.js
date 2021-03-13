62b528ca670896c92732ed875c88ddd877d9fc9d
Patrick Boutin <patrick.boutin@roberthalf.com>
2021-03-13 01:11:53 -0800

    Passed props and mapped them into functional component file.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -8,13 +8,13 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <Card />
+            <Card name="Jason Bourne" job="Spy" avatarId={51} />
           </div>
           <div className="col-md-4">
-            <Card />
+            <Card name="Jessica Jones" job="Private Investigator" avatarId={28} />
           </div>
           <div className="col-md-4">
-            <Card />
+            <Card name="Tom Hanks" job="Captain" avatarId={61} />
           </div>
         </div>
       </div>
--- first-react/src/components/Card.js
+++ first-react/src/components/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
+  const avatar = `https://i.pravatar.cc/150?img=${props.avatarId}`
   return (
     <div className="card">
-      <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
+      <img className="card-img-top" src={avatar} alt="Card image cap" />
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
+        <h5 className="card-title">{props.name}</h5>
         <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+        <a href="#" className="btn btn-primary">{props.job}</a>
       </div>
     </div>
   );
