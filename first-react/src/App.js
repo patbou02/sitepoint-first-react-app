b46383ebf3c5af2f0603ac4b662a016aa1d9f93a
Patrick Boutin <patrick.boutin@roberthalf.com>
2021-03-13 01:09:32 -0800

    Created functional Component Card.js, removed repeating code from App.js and imported component.

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -7,34 +8,13 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-md-4">
-            <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+            <Card />
           </div>
           <div className="col-md-4">
-            <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+            <Card />
           </div>
           <div className="col-md-4">
-            <div className="card">
-              <img className="card-img-top" src="https://i.pravatar.cc/150?img=3" alt="Card image cap" />
-              <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
+            <Card />
           </div>
         </div>
       </div>
